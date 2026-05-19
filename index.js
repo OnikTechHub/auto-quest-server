621d5ae38a8a6a4885b6ac9207beeac6e20d8edb
onik-999 <onikdas.dev@gmail.com>
2026-05-19 20:52:16 +0600

    implement Single Car Details API

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const dns = require('node:dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors'); 
 require('dotenv').config();
 
 const app = express();
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT 
 
 app.use(cors({
   origin: ["http://localhost:3000"], 
@@ -34,6 +34,8 @@ async function run() {
     const db = client.db("autoQuestDB");
     const carsCollection = db.collection("cars");
 
+//  Add Car API 1
+
     app.post("/api/cars", async (req, res) => {
       try {
         const newCar = req.body;
@@ -44,10 +46,31 @@ async function run() {
       }
     });
 
+    // Add Car API 2
+
     app.get("/api/cars", async (req, res) => {
       try {
         const result = await carsCollection.find().toArray();
         res.send({ success: true, data: result });
+      } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+      }
+    });
+
+    //  Single Car Details API
+
+    app.get("/api/cars/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        
+        const query = { _id: new ObjectId(id) };
+        const result = await carsCollection.findOne(query);
+        
+        if (result) {
+          res.send({ success: true, data: result });
+        } else {
+          res.status(404).send({ success: false, message: "Car not found" });
+        }
       } catch (error) {
         res.status(500).send({ success: false, message: error.message });
       }
